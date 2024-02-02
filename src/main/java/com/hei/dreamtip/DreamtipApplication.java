c5dab6a173360f4236d4b710edfe0a886e15fd2a
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2024-02-02 22:26:17 +0300

    initialize springboot application and add branch dev and preprod

---
+++ src/main/java/com/hei/dreamtip/DreamtipApplication.java
@@ -0,0 +1,13 @@
+package com.hei.dreamtip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamtipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamtipApplication.class, args);
+    }
+
+}
